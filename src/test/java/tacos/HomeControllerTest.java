ae63cf77f98b0c7c9791660d34646dba32a5a21f
fohic228335 <lisenchuk2000@mail.ru>
2025-07-16 15:14:45 +0300

    Version with hardcoded bd

--- src/test/java/tacos/HomeControllerTest.java
+++ src/test/java/tacos/HomeControllerTest.java
@@ -12,24 +12,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import tacos.web.HomeController;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(HomeController.class)   // <1>
+@WebMvcTest(HomeController.class)
 public class HomeControllerTest {
 
     @Autowired
-    private MockMvc mockMvc;   // <2>
+    private MockMvc mockMvc;
 
     @Test
     public void testHomePage() throws Exception {
-        mockMvc.perform(get("/"))    // <3>
+        mockMvc.perform(get("/"))
 
-                .andExpect(status().isOk())  // <4>
+                .andExpect(status().isOk())
 
-                .andExpect(view().name("home"))  // <5>
+                .andExpect(view().name("home"))
 
-                .andExpect(content().string(           // <6>
-                        containsString("Welcome to...")));
+                .andExpect(content().string(
+                        containsString("Welcome to")));
     }
 
 }
